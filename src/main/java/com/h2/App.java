a75d590380c8e2c4d2da088b979f200771fbf431
SpaghettiiMike <michalposluszny29@gmail.com>
2022-01-01 22:27:57 +0100

    Completed module

--- src/main/java/com/h2/App.java
+++ src/main/java/com/h2/App.java
@@ -3,7 +3,7 @@ package com.h2;
 
 /**
  * Hello world!
- *
+ *a
  */
 public class App 
 {
--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -14,6 +14,14 @@ public class BestLoanRates {
         System.out.println("Enter your name: ");
         String name = scanner.nextLine();
         System.out.println("Hello " + name +"!");
+        System.out.println("Enter the loan term (in years)");
+        int loanTermInYears = scanner.nextInt();
+        float bestRate = getRates(loanTermInYears);
+        if(bestRate == 0.0f) {
+            System.out.println("No available rates for ter: " + loanTermInYears + " years");
+        } else {
+            System.out.println("Best Available Rate: " + bestRate + "%");
+        }
     }
 
     public static float getRates(int loanTermInYears) {
